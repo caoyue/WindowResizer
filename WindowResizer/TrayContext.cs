74e4131c9e6d6e9bda5c4cc1583c410206c4faa6
Yue Cao <i@caoyue.me>
2016-07-12 16:29:58 +0800

    implement basic features

---
+++ WindowResizer/TrayContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowResizer.Properties;
+
+namespace WindowResizer {
+    public class TrayContext : ApplicationContext {
+        private readonly NotifyIcon _trayIcon;
+        private readonly KeyboardHook _hook = new KeyboardHook();
+
+        public TrayContext() {
+            try {
+                RegisterHotkey();
+            }
+            catch (Exception e) {
+                MessageBox.Show($"WindowResizer: register hotkey failed! Exception: {e.Message}");
+            }
+
+            _trayIcon = new NotifyIcon {
+                Icon = Resources.AppIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true,
+                Text = "WindowResizer"
+            };
+        }
+
+        private void Exit(object sender, EventArgs e) {
+            _trayIcon.Visible = false;
+            _hook.Dispose();
+            Application.Exit();
+        }
+
+        private void RegisterHotkey() {
+            _hook.KeyPressed += OnKeyPressed;
+            _hook.RegisterHotKey(ModifierKeys.Control | ModifierKeys.Alt, Keys.K);
+        }
+
+        void OnKeyPressed(object sender, KeyPressedEventArgs e) {
+            WindowControl.MoveWindow();
+        }
+    }
+}
---
+++ WindowResizer/WindowControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WindowResizer
+{
+    public static class WindowControl
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+
+        public static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            var buff = new StringBuilder(nChars);
+            var handle = GetForegroundWindow();
+
+            return GetWindowText(handle, buff, nChars) > 0 ? buff.ToString() : null;
+        }
+
+        public static void MoveWindow()
+        {
+            var handle = GetForegroundWindow();
+            if (handle != IntPtr.Zero) {
+                SetWindowPos(handle, 0, 0, 0, 600, 400, 0x0200);
+            }
+        }
+    }
+}
