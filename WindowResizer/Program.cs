7dc52b5fe6d9258d45783aa97ecb26141338426b
caoyue <i@caoyue.me>
2017-06-17 23:27:13 +0800

    hidpi compatible

--- WindowResizer/Program.cs
+++ WindowResizer/Program.cs
@@ -19,11 +19,20 @@ namespace WindowResizer
                     return;
                 }
 
+                if (Environment.OSVersion.Version.Major >= 6)
+                {
+                    SetProcessDPIAware();
+                }
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new TrayContext());
-            }
 
+            }
         }
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
