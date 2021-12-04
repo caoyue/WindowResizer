ee8deb5517e5861dbd048604f272c4889bfc7b14
caoyue <i@caoyue.me>
2021-12-04 18:01:01 +0800

    feat: restore window position automatically

---
+++ WindowResizer/WindowEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Automation;
+
+namespace WindowResizer
+{
+    public class WindowEventHandler
+    {
+        private static AutomationEventHandler _eventHandler = null;
+
+        public WindowEventHandler(Action<IntPtr> windowCreatedHandler)
+        {
+            _eventHandler = (sender, e) =>
+            {
+                AutomationElement src = sender as AutomationElement;
+                if (src != null)
+                {
+                    Console.WriteLine("Class : " + src.Current.ClassName);
+                    Console.WriteLine("Title : " + src.Current.Name);
+                    Console.WriteLine("Handle: " + src.Current.NativeWindowHandle);
+                    windowCreatedHandler(new IntPtr(src.Current.NativeWindowHandle));
+                }
+            };
+        }
+
+        public void AddWindowCreateHandle() =>
+            Automation.AddAutomationEventHandler(
+                WindowPattern.WindowOpenedEvent,
+                AutomationElement.RootElement,
+                TreeScope.Subtree,
+                _eventHandler
+            );
+
+        public void RemoveWindowCreateHandle()
+        {
+            Automation.RemoveAutomationEventHandler(
+                WindowPattern.WindowOpenedEvent,
+                AutomationElement.RootElement,
+                _eventHandler
+            );
+        }
+    }
+}
