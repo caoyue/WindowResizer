44dcdfebefde17e8f526afbda2806a9a359d446a
caoyue <i@caoyue.me>
2021-12-29 16:49:01 +0800

    feat: auto updater

---
+++ Installer/build.ps1
@@ -0,0 +1,70 @@
+New-Alias squirrel .\packages\squirrel.windows.2.0.1\tools\Squirrel.exe
+
+$version=$args[0]
+
+if ($version -notmatch '\d\.\d\.\d')
+{
+    Write-Output "Error: Version not set correctly."
+    exit 1
+}
+
+function patchAssembly($patchVersion)
+{
+    $pattern = '^\[assembly: AssemblyVersion\("(.*)"\)\]$'
+    $AssemblyFiles = Get-ChildItem . AssemblyInfo.cs -rec
+
+    foreach ($file in $AssemblyFiles)
+    {
+        (Get-Content $file.PSPath) | ForEach-Object{
+            if($_ -match $pattern)
+            {
+                '[assembly: AssemblyVersion("{0}")]' -f $patchVersion
+            } else
+            {
+                $_
+            }
+        } | Set-Content $file.PSPath
+    }
+}
+
+function patchFile($patchVersion)
+{
+    $pattern = '^\[assembly: AssemblyFileVersion\("(.*)"\)\]$'
+    $AssemblyFiles = Get-ChildItem . AssemblyInfo.cs -rec
+
+    foreach ($file in $AssemblyFiles)
+    {
+        (Get-Content $file.PSPath) | ForEach-Object{
+            if($_ -match $pattern)
+            {
+                '[assembly: AssemblyFileVersion("{0}")]' -f $patchVersion
+            } else
+            {
+                $_
+            }
+        } | Set-Content $file.PSPath
+    }
+}
+
+Write-Output ">> current version: ", $version
+
+# assembly info
+Write-Output ">> patching assembly info..."
+patchAssembly($version)
+patchFile($version)
+
+# build
+Write-Output ">> building..."
+nuget restore .\WindowResizer.sln
+msbuild .\WindowResizer\WindowResizer.csproj /p:Configuration=Release
+
+# nuget pack
+Write-Output ">> packing..."
+Copy-Item .\Installer\AppIcon.png .\WindowResizer\bin\Release\AppIcon.png
+Nuget pack .\Installer\WindowResizer.nuspec -Version $version -Properties Configuration=Release -BasePath .\WindowResizer\bin\Release -OutputDirectory  .\publish
+
+# squirrel release
+Write-Output ">> releasing..."
+squirrel  --setupIcon .\WindowResizer\Resources\AppIcon.ico --shortcut-locations 'StartMenu' --releasify .\publish\WindowResizer.$version.nupkg --no-msi
+
+Write-Output ">> done."
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # WindowResizer
 
-[![Build status](https://ci.appveyor.com/api/projects/status/xpetb3331p238qx8?svg=true)](https://ci.appveyor.com/project/caoyue/windowresizer)
+![WindowResizer](https://github.com/caoyue/WindowResizer/actions/workflows/WindowsResizer.yml/badge.svg)
 
-a simple tool gives you a way to quickly reset window size with a hotkey
+a simple tool gives you a way to quickly reset window size and position with a hotkey.
 
-you can save and restore window position for different process
+you can save and restore window position for different process.
 
 ## support
 
--   .NET Framework 4.5.2+
+-   .NET Framework 4.7.2
 
 ## hotkey
 
